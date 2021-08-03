d2d73cd66dffeb38787d2eef2d4786f25d80daf3
josecervan <cervnj@gmail.com>
2021-08-03 17:03:29 +0200

    docker setup

--- app/pintxos_django/settings.py
+++ app/pintxos_django/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*@_y$@k66+cx5y#4hk(1(e2wiz0$)h!kf04fwo*y_$95=kdz4_'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
